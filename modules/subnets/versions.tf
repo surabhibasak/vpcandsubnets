f0d87a49bea8265bc5e5aef5ee10f34d1a6dc8a2
surabhibasak <94508069+surabhibasak@users.noreply.github.com>
2022-03-02 17:26:51 +0530

    Update versions.tf

--- modules/subnets/versions.tf
+++ modules/subnets/versions.tf
@@ -17,14 +17,5 @@
 terraform {
   required_version = ">= 0.13.0"
 
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "<5.0,>= 2.12"
-    }
-  }
-
-  provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-network:subnets/v5.0.0"
-  }
+  
 }
