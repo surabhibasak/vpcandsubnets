9df877f02edd7fb00f9375a95aad82eebedf310d
surabhibasak <94508069+surabhibasak@users.noreply.github.com>
2022-03-02 17:25:33 +0530

    Update versions.tf

--- modules/firewall-rules/versions.tf
+++ modules/firewall-rules/versions.tf
@@ -17,14 +17,4 @@
 terraform {
   required_version = ">= 0.13.0"
 
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "<5.0,>= 2.12"
-    }
-  }
-
-  provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-network:firewall-rules/v5.0.0"
-  }
 }
