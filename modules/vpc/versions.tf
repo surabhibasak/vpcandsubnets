13279516cdafbf40b7efa7c7638da38a706a3880
surabhibasak <94508069+surabhibasak@users.noreply.github.com>
2022-03-02 17:27:27 +0530

    Update versions.tf

--- modules/vpc/versions.tf
+++ modules/vpc/versions.tf
@@ -17,21 +17,5 @@
 terraform {
   required_version = ">= 0.13.0"
 
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 3.45"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = ">= 3.45"
-    }
-  }
-
-  provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-network:vpc/v5.0.0"
-  }
-  provider_meta "google-beta" {
-    module_name = "blueprints/terraform/terraform-google-network:vpc/v5.0.0"
-  }
+ 
 }
